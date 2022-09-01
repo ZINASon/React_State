60a3036f3bd1206593851a9d586166ef932d6fcf
ZINASon <zinasondes@gmail.com>
2022-09-01 18:53:57 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import MyPhoto from "./sondes.jpg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Profession: "FullStack Developer",
+      fullName: "Zina Sondes",
+      bio: "Computer sciences engineer",
+      Image: MyPhoto,
+      show: false
+    }
+  };
+  handleClick = () => {
+    this.setState({ show: !this.state.show });
+  };
+  render() {
+    return (
+      <div>
+        {this.state.show && (
+          <h1>My Name is {this.state.fullName},<br></br>
+            My Profession is {this.state.Profession} and i'm a {this.state.bio}
+            <br></br>
+            Here is my Photo
+            <br></br>
+            <img width={200} height={300}
+              src={this.state.Image} alt='monphoto'></img>
+            {this.state.check}
+          </h1>)}
+        <button type="button" onClick={this.handleClick}>
+          Show Me
+        </button>
+        <br></br>
+        <h4>time for is:
+          componentDidMount(){ 
+            setInterval(() => this.render(), 1000)
+          }
+        </h4>
+      </div>
+    )
+
+  }
 }
-
 export default App;
